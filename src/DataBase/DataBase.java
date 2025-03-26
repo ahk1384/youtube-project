62795c99fc5a18fe8f2d04addb37e234c1ce13ef
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-26 13:00:00 +0330

    create DataBase class

--- src/DataBase/DataBase.java
+++ src/DataBase/DataBase.java
@@ -1,4 +1,13 @@
 package DataBase;
 
+import Account.Report;
+import Account.User;
+import content.Content;
+
+import java.util.ArrayList;
+
 public class DataBase {
+    private ArrayList<User> users;
+    private ArrayList<Content> contents;
+    private ArrayList<Report> reports;
 }
