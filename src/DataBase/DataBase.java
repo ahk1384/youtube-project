99e7113cccaec7af612e0314cd3698f98242c8e8
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-26 14:33:31 +0330

    add add & remove method to DataBase

--- src/DataBase/DataBase.java
+++ src/DataBase/DataBase.java
@@ -7,7 +7,25 @@ import content.Content;
 import java.util.ArrayList;
 
 public class DataBase {
-    private ArrayList<User> users;
-    private ArrayList<Content> contents;
-    private ArrayList<Report> reports;
+    private static ArrayList<User> users = new ArrayList<>();
+    private static ArrayList<Content> contents = new ArrayList<>();
+    private static ArrayList<Report> reports = new ArrayList<>();
+    public static void addReport(Report report) {
+        reports.add(report);
+    }
+    public static void addContent(Content content){
+        contents.add(content);
+    }
+    public static void addUser(User user){
+        users.add(user);
+    }
+    public static void removeUser(User user){
+        users.remove(user);
+    }
+    public static void removeContent(Content content){
+        contents.remove(content);
+    }
+    public static void removeReport(Report report){
+        reports.remove(report);
+    }
 }
