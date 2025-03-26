5319c57137c6abdd12ce0c5628967c7631be05fe
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-26 12:22:23 +0330

    compelete report class

--- src/Account/report.java
+++ src/Account/report.java
@@ -1,5 +1,6 @@
 package Account;
-
+import DataBase.DataBase;
+import Account.Account;
 public class report {
     public void report(Account.,int contentId,int reportId,String description){
         DataBase.DataBase.report(contentId,reportId,description);
