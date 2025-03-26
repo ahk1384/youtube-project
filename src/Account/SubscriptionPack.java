b575ca0633a508c61651860f7c0e9771be011b01
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-26 13:02:25 +0330

    create SubscriptionPack enum

--- src/Account/SubscriptionPack.java
+++ src/Account/SubscriptionPack.java
@@ -1,4 +1,11 @@
 package Account;
 
 public enum SubscriptionPack {
+    Gold(180,14),Silver(60 ,9),Bronze(30,5);
+    private int price;
+    private int allowedDays;
+    SubscriptionPack(int allowedDays , int price) {
+        this.price = price;
+        this.allowedDays = allowedDays;
+    }
 }
