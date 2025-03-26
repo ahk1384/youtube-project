c5bd40f75b350b8e1c47bd756ded305e11b5493f
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-26 12:27:29 +0330

    create Account class

--- src/Account/Account.java
+++ src/Account/Account.java
@@ -1,4 +1,11 @@
 package Account;
-
-public class Account {
+import content.*;
+import Channel.*;
+abstract public class Account {
+    private String userName;
+    private String password;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String profileCover;
 }
