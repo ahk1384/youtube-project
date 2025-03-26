b820e2ad2806312521c911d643c902cd6699e2ff
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-26 12:41:23 +0330

    create comment class

--- src/content/comment.java
+++ src/content/comment.java
@@ -1,4 +1,11 @@
 package content;
 
+import Account.User;
+
+import java.util.Date;
+
 public class comment {
+    private User userCommenter;
+    private String comment;
+    private Date commentDate;
 }
