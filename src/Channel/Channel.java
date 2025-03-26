fc1957745ba9dbdadba4d9538105dba4e81d1b29
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-26 12:59:33 +0330

    create Channel class

--- src/Channel/Channel.java
+++ src/Channel/Channel.java
@@ -1,4 +1,17 @@
 package Channel;
 
+import Account.User;
+import Account.playlist;
+
+import java.util.ArrayList;
+
 public class Channel {
+    private int channelId;
+    private String channelName;
+    private String channelDescription;
+    private String channelCover;
+    private String channelOwner;
+    private ArrayList<playlist> playlists;
+    private ArrayList<User> subscribers;
+
 }
