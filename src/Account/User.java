3976cd29fc7d1cbba94f2aaa398bc58edb6956af
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-26 12:39:10 +0330

    compelete user class

--- src/Account/User.java
+++ src/Account/User.java
@@ -1,16 +1,20 @@
 package Account;
-
 import Channel.Channel;
-import jdk.jshell.JShell;
-
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Locale;
-
 public class User extends Account {
     private int credit ;
     playlist[] playlists ;
     private Channel channel ;
     private ArrayList<Channel> subscriptions ;
     private ArrayList<Category> likedCategory;
-    
+
+}
+class NormalUser extends User {
+    private final int maxPlaylist = 5;
+    private final int maxContent = 5;
+}
+class PremiumUser extends User {
+    private Date expirationDate;
 }
