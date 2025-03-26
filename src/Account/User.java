6db95918a2d4ef6a1892d0ba44011ed925616f2c
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-26 12:34:43 +0330

    create user class

--- src/Account/User.java
+++ src/Account/User.java
@@ -1,4 +1,16 @@
 package Account;
 
-public class User {
+import Channel.Channel;
+import jdk.jshell.JShell;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class User extends Account {
+    private int credit ;
+    playlist[] playlists ;
+    private Channel channel ;
+    private ArrayList<Channel> subscriptions ;
+    private ArrayList<Category> likedCategory;
+    
 }
