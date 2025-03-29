19980d07288f25a5ab3bea479db59218b2ff9a0b
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-29 11:49:55 +0330

    correct report class

--- src/Model/Content/Report.java
+++ src/Model/Content/Report.java
@@ -5,12 +5,12 @@ import Model.Account.User;
 public class Report {
     private static int idCounter = 1;
     private int reportId;
-    private User reporter;
+    private int reporterId;
     private int userReportedID;
     private int contentReportedID;
     private String description;
-    public User getReporter() {
-        return reporter;
+    public int getReporterId() {
+        return reporterId;
     }
 
     public int getUserReportedID() {
@@ -33,9 +33,9 @@ public class Report {
         this.description = description;
     }
 
-    public Report(User reporter, int userReportedID, int contentReportedID, String description) {
+    public Report(int reporterId, int userReportedID, int contentReportedID, String description) {
         this.reportId = idCounter++;
-        this.reporter = reporter;
+        this.reporterId = reporterId;
         this.userReportedID = userReportedID;
         this.contentReportedID = contentReportedID;
         this.description = description;
