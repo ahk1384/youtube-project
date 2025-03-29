d257e0779eb7a3fa78965fb0ce96c614c241c55d
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-29 11:52:49 +0330

    correct podcast class

--- src/Model/Content/Podcast.java
+++ src/Model/Content/Podcast.java
@@ -1,14 +1,14 @@
 package Model.Content;
 
+import Controller.DataBase.DataBaseController;
 import Model.Account.User;
 import Model.Account.Category;
-import Model.DataBase.DataBase;
 
 public class Podcast extends Content {
     private User owner;
-    public Podcast(String contentName, Boolean isExlusive, String contentDescription, int playTime, int viewCount, int likeCount, Category category, String contentPath, String cover, int userId) {
-        super(contentName, isExlusive, contentDescription, playTime, viewCount, likeCount, category, contentPath, cover);
-        this.owner = DataBase.getUserById(userId);
+    public Podcast(int userId,int channelid ,String contentName, Boolean isExlusive, String contentDescription, int playTime, int viewCount, int likeCount, Category category, String contentPath, String cover) {
+        super(userId , channelid,contentName, isExlusive, contentDescription, playTime, viewCount, likeCount, category, contentPath, cover);
+        this.owner = DataBaseController.getUserById(userId);
     }
     public User getOwner() {
         return owner;
