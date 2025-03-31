3cd662cb7e5557f45c333c8b876bce68695c717a
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-31 20:32:49 +0330

    create other content classes

--- src/Model/Content/Podcast.java
+++ src/Model/Content/Podcast.java
@@ -1,18 +1,21 @@
 package Model.Content;
 
 import Controller.DataBase.DataBaseController;
-import Model.Account.User;
 import Model.Account.Category;
+import Model.Account.User;
 
 public class Podcast extends Content {
     private User owner;
-    public Podcast(int userId,int channelid ,String contentName, Boolean isExlusive, String contentDescription, int playTime, int viewCount, int likeCount, Category category, String contentPath, String cover) {
-        super(userId , channelid,contentName, isExlusive, contentDescription, playTime, viewCount, likeCount, category, contentPath, cover);
+
+    public Podcast(int userId, int channelid, String contentName, Boolean isExlusive, String contentDescription, int playTime, Category category, String contentPath, String cover) {
+        super(userId, channelid, contentName, isExlusive, contentDescription, playTime, category, contentPath, cover);
         this.owner = DataBaseController.getUserById(userId);
     }
+
     public User getOwner() {
         return owner;
     }
+
     public void setOwner(User owner) {
         this.owner = owner;
     }
