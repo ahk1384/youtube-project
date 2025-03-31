c427af2b98d1309e0ce33115ddf0eead5f9d51ef
Amir Hossien Karimi <amirhkz1384@gmail.com>
2025-03-31 20:29:13 +0330

    correct enums

--- src/Model/Account/SubscriptionPack.java
+++ src/Model/Account/SubscriptionPack.java
@@ -1,16 +1,19 @@
 package Model.Account;
 
 public enum SubscriptionPack {
-    Gold(180,14),Silver(60 ,9),Bronze(30,5);
-    private int price;
-    private int allowedDays;
-    SubscriptionPack(int allowedDays , int price) {
+    Gold(180, 14), Silver(60, 9), Bronze(30, 5);
+    private final int price;
+    private final int allowedDays;
+
+    SubscriptionPack(int allowedDays, int price) {
         this.price = price;
         this.allowedDays = allowedDays;
     }
+
     public int getPrice() {
         return price;
     }
+
     public int getAllowedDays() {
         return allowedDays;
     }
---
+++ src/Model/Content/Quality.java
@@ -0,0 +1,5 @@
+package Model.Content;
+
+public enum Quality {
+    P360, P720, P1080
+}
